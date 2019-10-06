98025b5f9a94ecd976c77498c1085fee78169c87
Pritesh Gudge <priteshgudge@gmail.com>
2019-10-07 00:48:46 +0530

    Functional component for App with SetState Hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 
 import Person from './Person/Person'
-
+import './Person/Person.css'
 function App() {
+  
+  const [state, setState] = useState({persons:[{name: "Pritesh", age:"28"}]});
+  const switchNameHandler = () => {
+    if (state.persons[0].age === "28"){
+    setState({...state, persons:[{name: "New Pritesh", age:"29"}]})
+    }else{
+      setState({...state, persons:[{name: "Pritesh", age:"28"}]})
+    }
+  } 
   return (
     <div className="App">
      <h1>Hi</h1>
-     <Person name="Pritesh" age="28"/>
+     <Person name={state.persons[0].age} age={state.persons[0].name}/>
      <Person name="Other Person" age="30"> Other Text </Person>
+     <button onClick={switchNameHandler}>Switch Name</button>
     </div>
   );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const person = (props) => {
     return (
-    <div>
+    <div className="Person">
         <p>I am a {props.name} and I am {props.age}  years old!</p>
         <p>{props.children}</p>
     </div>
